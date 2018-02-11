0a08fc3979eaa0dbb121a63c2080b3c183c3b291
Ostap Chervak <ostap.chervak@strikersoft.com>
2018-02-12 00:16:32 +0200

    Reintroduce i18n

---
+++ packages/poa-i18n/index.d.ts
@@ -0,0 +1,9 @@
+import * as i18next from 'i18next';
+
+/**
+ * @private
+ * @param config
+ * @param componentsInjector
+ */
+export function boot(config: i18next.InitOptions, componentsInjector): Promise<void>;
+export const i18n: i18next.i18n;
---
+++ packages/poa-i18n/src/boot.js
@@ -0,0 +1,30 @@
+// @ts-check
+
+import i18next from 'i18next';
+
+/**
+ * @private
+ * @param {*} config
+ * @param {*} componentsInjector
+ */
+export function boot(config, componentsInjector) {
+  return new Promise((resolve, reject) => {
+    i18next.init(config, (error, t) => {
+      if (error) {
+        return reject(error);
+      }
+
+      componentsInjector(component => {
+        component.prototype.t = translatorFuncCreator(component.prototype.tNamespaces || [], t);
+      });
+
+      return resolve(t);
+    });
+  });
+}
+
+export const i18n = i18next;
+
+const translatorFuncCreator = (dicts = [], t) => (value, opts = {}) => {
+  return t(value, { ns: [...dicts], ...opts });
+};
