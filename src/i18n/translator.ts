908563558997b19ea3ba0da393da4a4b16f95cd5
Ostap Chervak <ostap.chervak@strikersoft.com>
2018-07-14 22:54:39 +0300

    Fix translator

--- src/i18n/translator.ts
+++ src/i18n/translator.ts
@@ -1,8 +1,8 @@
 import { TranslationFunction, TranslationOptions } from 'i18next';
 
 export function createTranslator(dicts = [], t: TranslationFunction) {
-  function translator(value: string | string[], opts: TranslationOptions) {
-    return t(value, { ns: [...dicts], opts });
+  function translator(value: string | string[], opts: TranslationOptions = {}) {
+    return t(value, { ns: [...dicts], ...opts });
   }
 
   return translator;
