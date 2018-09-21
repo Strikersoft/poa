dc9a305d0ffb2d383ce29b50a5baec2a2a490c31
Ostap Chervak <ostap.chervak@strikersoft.com>
2018-09-21 17:44:46 +0300

    Published version 6.0.0-beta23

--- src/core/interfaces/app-config.interface.ts
+++ src/core/interfaces/app-config.interface.ts
@@ -1,6 +1,7 @@
 import { InitOptions, i18n, TranslationFunction } from 'i18next';
 import { IReactComponent } from 'mobx-react';
 import { RavenOptions } from 'raven-js';
+import { Location } from 'history';
 
 export interface Route {
   context: any;
@@ -9,6 +10,7 @@ export interface Route {
 
 export interface Navigation {
   redirectTo: (path: string) => any;
+  to: Location;
   [key: string]: any;
 }
 
